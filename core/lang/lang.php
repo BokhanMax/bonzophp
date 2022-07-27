539dd241056ee3778cdab6d4897a8f92e29a6365
Max Bokhan <bokhanworks@gmail.com>
2022-07-28 01:16:48 +0300

    additionals to multilang system

--- core/lang/lang.php
+++ core/lang/lang.php
@@ -4,5 +4,7 @@ if (!isset($_SESSION["lang"])) { $_SESSION["lang"] = "uk"; }
 if (isset($_POST["lang"])) { $_SESSION["lang"] = $_POST["lang"]; }
 
 require "lang-common-" . $_SESSION["lang"] . ".php";
+require "lang-pages-" . $_SESSION["lang"] . ".php";
+require "lang-posts-" . $_SESSION["lang"] . ".php";
+require "lang-widgets-" . $_SESSION["lang"] . ".php";
 require "lang-seo-" . $_SESSION["lang"] . ".php";
-

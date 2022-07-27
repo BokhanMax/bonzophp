f5f6cc6d8d6b33a0740d65c58558f39d55e5ac1b
Max Bokhan <bokhanworks@gmail.com>
2022-07-27 04:35:31 +0300

    test assetsDir in homepage

--- views/pages/home.tpl.php
+++ views/pages/home.tpl.php
@@ -1,3 +1,7 @@
 <main>
-
+	<div class="columns">
+		<div class="column" style="text-align: center; margin-top: 5%;">
+			<img src="<? echo $assetsDir; ?>/img/webpr.jpg" alt="Webpr">
+		</div>
+	</div>
 </main>
